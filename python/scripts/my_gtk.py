54ab932047f788f0caea209053c3cb3223c615f6
Markham <quinesquines@gmail.com>
2018-02-22 13:07:31 -0800

    color flashes added to my_gtk

--- python/scripts/my_gtk.py
+++ python/scripts/my_gtk.py
@@ -2,13 +2,17 @@ import sys
 import pygtk
 pygtk.require('2.0')
 import gtk
+import gobject
 
 ROW_CT = 2
 COL_CT = 2
-FLASH_COLOR = gtk.gdk.Color(0,65535,65535)
-TARGET_COLOR = gtk.gdk.Color(65535,0,65535)
-FLASH_DURATION = 1
-TARGET_DURATION = 1
+NULL_FG = gtk.gdk.Color(20000,20000,20000)
+NULL_BG = gtk.gdk.Color(60535,60535,60535)
+FLASH_BG = gtk.gdk.Color(0,0,0)
+FLASH_FG = gtk.gdk.Color(60535,60535,60535)
+TARGET_BG = gtk.gdk.Color(25535,0,25535)
+FLASH_DURATION = 250
+TARGET_DURATION = 250
 
 CHARS = [ chr(x) for x in range(65,91) ] + [ chr(x) for x in range(48,58) ]
 
@@ -23,9 +27,13 @@ def make_label():
 
 class Base:
   def __init__(self):
+    # Non-gui attrs
+    self.timer = None
+    # Gui attrs
     self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
     if 'openvibe' not in sys.argv:
-      self.window.connect("destroy", self.destroy)
+      self.window.connect("delete_event", self._delete_event)
+      self.window.connect("destroy", self._destroy)
     self.window.connect("key-press-event", self._key_press_event)
     self.lbls = [make_label() for i in range(4)]
     # Table
@@ -45,11 +53,12 @@ class Base:
     self.vbox.pack_end(self.target, expand=False, fill=True)
     self.vbox.show()
     # Window
-    self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
     self.window.resize(600,600)
     self.window.add(self.vbox)
     self.window.show()
     self._set_view(())
+    self._restore_bg()
+    self.lbls[1].get_children()[0].modify_fg(gtk.STATE_NORMAL, NULL_FG)
 
   def _get_chars(self, selections):
     threes = [CHARS[i:i+3] for i in range(0, len(CHARS), 3)]
@@ -74,36 +83,76 @@ class Base:
   def start(self, target_text):
     self.target_text = target_text
 
-  def _highlight_cell(self, row, col, color, duration):
+  def _restore_bg(self):
+    for lbl in self.lbls:
+      lbl.modify_bg(gtk.STATE_NORMAL, NULL_BG)
+      lbl.get_children()[0].modify_fg(gtk.STATE_NORMAL, NULL_FG)
+
+
+  def _highlight_cell(self, row, col, bg_color, fg_color=None):
     i = COL_CT * row + col
-    print 'highlight cell'
-    self.lbls[i].modify_bg(gtk.STATE_NORMAL, FLASH_COLOR)
+    self.lbls[i].modify_bg(gtk.STATE_NORMAL, bg_color)
+    if fg_color != None:
+      self.lbls[i].get_children()[0].modify_fg(gtk.STATE_NORMAL, fg_color)
 
   def highlight_row(self, row):
     for col in range(COL_CT):
-      self._highlight_cell(row, col, FLASH_COLOR, FLASH_DURATION)
+      self._highlight_cell(row, col, FLASH_BG, FLASH_FG)
+    gobject.timeout_add(FLASH_DURATION, self._restore_bg)
 
   def highlight_col(self, col):
     for row in range(ROW_CT):
-      self._highlight_cell(row, col, FLASH_COLOR, FLASH_DURATION)
+      self._highlight_cell(row, col, FLASH_BG, FLASH_FG)
+    gobject.timeout_add(FLASH_DURATION, self._restore_bg)
 
   def highlight_target(self, row, col):
-    self._highlight_cell(row, col, TARGET_COLOR, TARGET_DURATION)
+    self._highlight_cell(row, col, TARGET_BG)
+    gobject.timeout_add(TARGET_DURATION, self._restore_bg)
 
-  def main(self):
-    gtk.main()
-
-  def destroy(self, widget, data=None):
+  def _destroy(self, widget, data=None):
+    print('quitting')
     gtk.main_quit()
+
+  def _delete_event(self, widget, event, data=None):
+    gtk.main_quit()
+    return gtk.FALSE
 
   def _key_press_event(self, widget, event):
     print('key pressed!')
     name = gtk.gdk.keyval_name(event.keyval)
     if name == 'q':
-      self.destroy()
+      self._destroy(widget, event)
+    elif name == 't':
+      self.highlight_row(0)
+    elif name == 'b':
+      self.highlight_row(1)
+    elif name == 'l':
+      self.highlight_col(0)
+    elif name == 'r':
+      self.highlight_col(1)
+    elif name == 'w':
+      self.highlight_target(0, 0)
+    elif name == 'a':
+      self.highlight_target(1, 0)
+    elif name == 's':
+      self.highlight_target(0, 1)
+    elif name == 'd':
+      self.highlight_target(1, 1)
+    elif name == '1':
+      self._set_view(())
+    elif name == '2':
+      self._set_view((1,))
+    elif name == '3':
+      self._set_view((1,2))
     else:
       print name
+
+  def main(self):
+    gtk.main()
+
 
 if __name__ == "__main__" and 'openvibe' not in sys.argv:
   base = Base()
   base.main()
+
+  print('end')
